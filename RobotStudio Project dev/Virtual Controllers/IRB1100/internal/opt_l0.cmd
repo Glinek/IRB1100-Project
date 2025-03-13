f149fb2e40099b23feacd1e56c651e37aa49eda4
Szymon Glinek Glinka <szymonglinka2006@gmail.com>
2025-03-13 19:12:45 +0100

    lfs prune

---
+++ RobotStudio Project dev/Virtual Controllers/IRB1100/internal/opt_l0.cmd
@@ -0,0 +1,4 @@
+
+include -path "C:/Users/szymo/AppData/Local/ABB/RobotWare/RobotControl_7.14.3/options/vnet/virtualInc_opt.cmd"
+
+include -path "C:/Users/szymo/AppData/Local/ABB/RobotWare/RobotControl_7.14.3/options/sc/ScInc_opt.cmd"
